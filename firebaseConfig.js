476755da14face3050ff07a47ada936547094e3e
JenniEmilia <jenni.jarvinen2@myy.haaga-helia.fi>
2025-11-07 08:16:29 +0200

    Lisätty firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDeY_MXlajO6pPeov9A8gtxPqFhPBuOrz4',
+  authDomain: 'pelikirjasto-78f68.firebaseapp.com',
+  databaseURL: 'https://pelikirjasto-78f68-default-rtdb.firebaseio.com',
+  projectId: 'pelikirjasto-78f68',
+  storageBucket: 'pelikirjasto-78f68.firebasestorage.app',
+  messagingSenderId: '738064481651',
+  appId: '1:738064481651:web:d19d98a53d1fcd81f9d906',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
